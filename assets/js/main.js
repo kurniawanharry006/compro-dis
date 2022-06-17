3e10a0440dabf52f848ebfd1db1c50dc43a49468
Harry Kurniawan <53112352+kurniawanharry006@users.noreply.github.com>
2022-06-17 13:58:58 +0700

    tes2

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,19 +106,3 @@ sr.reveal(`.home__data, .home__img,
     interval: 200
 })
 
-var swiper = new Swiper(".mySwiper", {
-    effect: "coverflow",
-    grabCursor: true,
-    centeredSlides: true,
-    slidesPerView: "auto",
-    coverflowEffect: {
-      rotate: 50,
-      stretch: 0,
-      depth: 100,
-      modifier: 1,
-      slideShadows: true,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-  });
